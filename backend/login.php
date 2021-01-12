26cc81ae630f5e21cff887c51e11bdec174bf59d
Ritesh Rathod <rnrathod16@gmail.com>
2021-01-12 19:22:31 +0530

    First Push

---
+++ backend/login.php
@@ -0,0 +1,35 @@
+<?php
+
+include "dbconnect.php";
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+    $sql="SELECT * FROM `user` WHERE `email`='$email'";
+
+    $result=mysqli_query($con,$sql);
+
+    $num=mysqli_num_rows($result);
+
+    if($num==1){
+        while($fetch=mysqli_fetch_Assoc($result)){
+
+            if($fetch['email']==$email && $fetch['password']==$password){
+                session_start();
+                          $_SESSION['email']=$email;
+                          
+
+                echo "<script>window.location.href='user.php'</script>";
+            }
+            else{
+                echo "Invalid crentials";
+            }
+        }
+
+    }
+}
+
+
+?>
