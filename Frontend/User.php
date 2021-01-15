2b592fd89978bacf4d823017011e8764620b1a12
Mohit Shende <mohitshende023@gmail.com>
2021-01-15 19:30:41 +0530

    changed add post section

--- Frontend/User.php
+++ Frontend/User.php
@@ -93,17 +93,11 @@
             </svg>
           </button>
         </div>
-        <div class="flex">
-          <a
-            href="#"
-            class="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0"
-            >Sign in</a
-          >
 
           <a
             href="#"
             class="block text-md px-4 ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0"
-            >login</a
+            >Logout</a
           >
         </div>
       </div>
@@ -138,15 +132,6 @@
           <section class="text-gray-600 body-font relative">
             <div class="container px-5 py-24 mx-auto">
               <div class="flex flex-col text-center w-full mb-12">
-                <h1
-                  class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
-                >
-                  Contact Us
-                </h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-                  Whatever cardigan tote bag tumblr hexagon brooklyn
-                  asymmetrical gentrify.
-                </p>
               </div>
               <form method="post">
               
@@ -155,21 +140,17 @@
                   </div>
                   <div class="p-2 w-full">
                     <div class="relative">
-                      <label
-                        for="message"
-                        class="leading-7 text-sm text-gray-600"
-                        >Message</label
-                      >
                       <textarea
                         id="message"
                         name="message"
+                        placeholder="Write what you feel like..."
                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                       ></textarea>
                     </div>
                   </div>
                   <div class="p-2 w-full">
                     <button type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" name="post">
-                      Button
+                      Post
                     </button>
                   </div>
                   </div>
