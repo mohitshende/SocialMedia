f7c8c8f7c5d6e5aa25f32bb15e897d101e09ad7f
Ritesh Rathod <rnrathod16@gmail.com>
2021-01-20 12:05:40 +0530

    Added new Feature to Upload the Images and Dispaly it

--- backend/post.php
+++ backend/post.php
@@ -1,5 +1,8 @@
 <?php
-
+error_reporting(0); 
+?> 
+<?php
+$msg = ""; 
 include "dbconnect.php";
 session_start();
 $set=$_SESSION['email'];
@@ -7,12 +10,22 @@ $set=$_SESSION['email'];
 
 if(isset($_POST['post'])){
     // session_start();
-    
+    $filename = $_FILES["uploadfile"]["name"]; 
+    $tempname = $_FILES["uploadfile"]["tmp_name"];     
+    $folder = "UserImages/".$filename; 
     $message=$_POST['message'];
-    $sql="INSERT INTO `$set` ( `message`, `time`) VALUES ( '$message', current_timestamp());";
-
+    
+    
+    $sql="INSERT INTO `$set` (`message`,`filename`, `time`) VALUES ('$message','$filename', current_timestamp());";
     $result = mysqli_query($con,$sql);
+    
+    if (move_uploaded_file($tempname, $folder))  { 
+        $msg = "Image uploaded successfully"; 
+    }else{ 
+        $msg = "Failed to upload image"; 
+  } 
    
+  
 }
 
 
