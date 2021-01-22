b0e5e5ad2c652922de8908d6348f4361850187ed
Ritesh Rathod <rnrathod16@gmail.com>
2021-01-22 13:30:37 +0530

    Changed the name of the file that is beign Uploaded

--- backend/post.php
+++ backend/post.php
@@ -11,21 +11,30 @@ $set=$_SESSION['email'];
 if(isset($_POST['post'])){
     // session_start();
     $filename = $_FILES["uploadfile"]["name"]; 
-    $tempname = $_FILES["uploadfile"]["tmp_name"];     
-    $folder = "UserImages/".$filename; 
+    $tempname = $_FILES["uploadfile"]["tmp_name"]; 
+    mkdir("UserImages/$set");
+    $folder = "UserImages/".$set."/"; 
+    
+    $newfileName=date('dmyHis').str_replace(" ","",basename($filename));
+    
+
     $message=$_POST['message'];
+    $heading=$_POST['heading'];
     
     
-    $sql="INSERT INTO `$set` (`message`,`filename`, `time`) VALUES ('$message','$filename', current_timestamp());";
+    
+    // $sql="INSERT INTO `$set` (`message`,`filename`, `time`) VALUES ('$message','$newfileName', current_timestamp());";
+    $sql="INSERT INTO `$set` (`heading`, `message`, `filename`, `time`) VALUES ('$heading', '$message', '$newfileName', current_timestamp());";
+    
     $result = mysqli_query($con,$sql);
     
-    if (move_uploaded_file($tempname, $folder))  { 
+    
+    if (move_uploaded_file($tempname, $folder.$newfileName))  { 
         $msg = "Image uploaded successfully"; 
     }else{ 
         $msg = "Failed to upload image"; 
   } 
    
-  
 }
 
 
